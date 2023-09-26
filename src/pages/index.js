cd18b715e9f07008f8d453abaf24d2dd8ac57fd7
bek77g <d.zheenkul@gmail.com>
2023-09-26 19:24:19 +0600

    init: index exports file

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Authorization from './Authorization/Authorization';
+import Home from './Home/Home';
+import Community from './Community/Community';
+import Profile from './Profile/Profile';
+
+export { Authorization, Home, Community, Profile };
