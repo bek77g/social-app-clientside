fd2b1ebe84f8f24a90fdc380ec73d9627ea9396b
bek77g <d.zheenkul@gmail.com>
2023-09-26 19:23:49 +0600

    add: Routes and base routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Layout } from './components';
+import { Authorization, Home } from './pages';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div className='main'></div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/auth' element={<Authorization />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
