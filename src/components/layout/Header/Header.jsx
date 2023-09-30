a851a7414e4f504c6789a34e226223116a232752
bek77g <d.zheenkul@gmail.com>
2023-09-30 16:32:17 +0600

    add: base template of header

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,7 +1,30 @@
+import { ReactComponent as VKLogo } from '../../../assets/vectors/content-logo.svg';
+
+//styles
 import styles from './Header.module.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container'>
+        <div className={styles.wrapper}>
+          <a className={styles.logo} href='#'>
+            <h1>
+              <VKLogo />
+            </h1>
+          </a>
+          <div className={styles.player}>PLAYER</div>
+          <div className={styles.profile}>
+            <h3>Elvis</h3>
+            <div className={styles.avatar}>
+              <img src='' alt='AVATAR' />
+              <span className={styles.dropdown}>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
