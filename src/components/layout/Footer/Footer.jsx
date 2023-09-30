3dd887b8125c2c6703d11a954d20737ff8ebbb24
bek77g <d.zheenkul@gmail.com>
2023-09-30 19:23:04 +0600

    add: full footer content

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,7 +1,42 @@
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className='container'>
+        <div className={styles.wrapper}>
+          <p className={styles.copyright}>
+            <span>VK</span>© 2006-2020
+          </p>
+          <ul className={styles.links}>
+            <li>
+              <a href='/'>About VK</a>
+            </li>
+            <li>
+              <a href='/'>Terms</a>
+            </li>
+            <li>
+              <a href='/'>Developers</a>
+            </li>
+          </ul>
+          <ul className={styles.links}>
+            <li>
+              <a href='/'>English</a>
+            </li>
+            <li>
+              <a href='/'>Русский</a>
+            </li>
+            <li>
+              <a href='/'>Українська</a>
+            </li>
+            <li>
+              <a href='/'>all languages »</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
