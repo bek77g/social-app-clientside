fa56d1a0475a081d5c4a97af9756144e2dfbcb6f
bek77g <d.zheenkul@gmail.com>
2023-09-30 19:09:36 +0600

    add: end profile block, searchbar, notify icon

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,4 +1,8 @@
 import { ReactComponent as VKLogo } from '../../../assets/vectors/content-logo.svg';
+import { InputField } from '../../ui';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { HiSearch } from 'react-icons/hi';
+import { FaBell } from 'react-icons/fa';
 
 //styles
 import styles from './Header.module.css';
@@ -8,17 +12,26 @@ const Header = () => {
     <header>
       <div className='container'>
         <div className={styles.wrapper}>
-          <a className={styles.logo} href='#'>
+          <a className={styles.logo} href='/'>
             <h1>
               <VKLogo />
             </h1>
           </a>
-          <div className={styles.player}>PLAYER</div>
+          <div className={styles.handlers}>
+            <label className={styles.search}>
+              <HiSearch color='#8fadc8' />
+              <InputField type='text' label='Поиск' />
+            </label>
+            <FaBell color='#1E3C5F' size='20' cursor='pointer' />
+          </div>
           <div className={styles.profile}>
             <h3>Elvis</h3>
             <div className={styles.avatar}>
-              <img src='' alt='AVATAR' />
-              <span className={styles.dropdown}>0</span>
+              <img
+                src='https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250'
+                alt='AVATAR'
+              />
+              <MdKeyboardArrowDown color='#fff' />
             </div>
           </div>
         </div>
