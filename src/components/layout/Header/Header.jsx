a50e7191ae218ea6a16bb8fce12d5b353f2c25e3
bek77g <d.zheenkul@gmail.com>
2023-10-03 19:06:43 +0600

    add: show user profile and login btn

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -6,8 +6,12 @@ import { FaBell } from 'react-icons/fa';
 
 //styles
 import styles from './Header.module.css';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const { user } = useSelector((state) => state.user);
+
   return (
     <header>
       <div className='container'>
@@ -25,14 +29,20 @@ const Header = () => {
             <FaBell color='#1E3C5F' size='20' cursor='pointer' />
           </div>
           <div className={styles.profile}>
-            <h3>Elvis</h3>
-            <div className={styles.avatar}>
-              <img
-                src='https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250'
-                alt='AVATAR'
-              />
-              <MdKeyboardArrowDown color='#fff' />
-            </div>
+            {!!user ? (
+              <>
+                <h3>{user.name}</h3>
+                <div className={styles.avatar}>
+                  <img
+                    src='https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250'
+                    alt='AVATAR'
+                  />
+                  <MdKeyboardArrowDown color='#fff' />
+                </div>
+              </>
+            ) : (
+              <Link to='/auth'>Вход</Link>
+            )}
           </div>
         </div>
       </div>
