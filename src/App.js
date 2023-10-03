5babdd7b8ccfecc065177f8f06e5e70509f85b7f
bek77g <d.zheenkul@gmail.com>
2023-10-03 19:07:00 +0600

    add: toastcontainer to globaly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { ToastContainer } from 'react-toastify';
 import { Layout } from './components';
 import { Authorization, Home } from './pages';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} /> Outlet if path === '/'
-      </Route>
-      <Route path='/auth' element={<Authorization />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} /> Outlet if path === '/'
+        </Route>
+        <Route path='/auth' element={<Authorization />} />
+      </Routes>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
 
 //Media styles
 import './index.css';
-import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
