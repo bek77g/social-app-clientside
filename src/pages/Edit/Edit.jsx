879d697cfd5e0a9b21e4d0f3be81e819f614f121
bek77g <d.zheenkul@gmail.com>
2023-10-07 17:56:16 +0600

    add: edit paga base template

--- src/pages/Edit/Edit.jsx
+++ src/pages/Edit/Edit.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import styles from './Edit.module.css';
 
 const Edit = () => {
-  return <div>Edit</div>;
+  const { user } = useSelector((state) => state.user);
+
+  console.log(user);
+
+  const fullName = `${user.name || ''} ${user.surname || ''}`;
+
+  return (
+    <>
+      <section>
+        <div className={styles.basic}>
+          <h4>Профиль</h4>
+          <div className={styles.cover}></div>
+          <div className={styles.info}>
+            <label className={styles.avatar}>
+              <img
+                src={
+                  user.avatar ||
+                  'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'
+                }
+                alt='AVATAR'
+              />
+              <input type='file' name='avatar' />
+            </label>
+            <p className={styles.fullname}>{fullName}</p>
+            <button>Cохранить</button>
+          </div>
+        </div>
+        <div className={styles.additional}></div>
+      </section>
+      <div className='aside-container'>COMMUNITY</div>
+    </>
+  );
 };
 
 export default Edit;
