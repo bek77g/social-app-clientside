29953b31441042af077149b68fdc7ff118df00b2
bek77g <d.zheenkul@gmail.com>
2023-10-12 14:23:57 +0600

    add: avatar changing and editing result toasting

--- src/pages/Edit/Edit.jsx
+++ src/pages/Edit/Edit.jsx
@@ -1,12 +1,41 @@
-import { useSelector } from 'react-redux';
 import styles from './Edit.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { changeUserAvatar, editUser } from 'redux/features/userSlice';
+import { useState } from 'react';
+import userPlaceholder from 'assets/vectors/user-placeholder.svg';
+import { uploadPhoto } from 'utils/uploadPhoto';
+import { toast } from 'react-toastify';
 
 const Edit = () => {
   const { user } = useSelector((state) => state.user);
-
-  console.log(user);
+  const [isEditable, setIsEditable] = useState(false);
+  const dispatch = useDispatch();
 
   const fullName = `${user.name || ''} ${user.surname || ''}`;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onSubmit' });
+
+  const handleEditUser = (data) => {
+    dispatch(editUser({ userId: user._id, data })).then(() =>
+      toast.success('Данные успешно изменены')
+    );
+    setIsEditable(false);
+  };
+
+  const handleUploadAvatar = async (e) => {
+    const selectedFile = e.target.files[0];
+    const uploadedPhotoUrl = await uploadPhoto(selectedFile);
+    dispatch(
+      changeUserAvatar({ avatarUrl: uploadedPhotoUrl, userId: user._id })
+    ).then(() => {
+      toast.success('Аватар успешно изменён');
+    });
+  };
 
   return (
     <>
@@ -16,17 +45,35 @@ const Edit = () => {
           <div className={styles.cover}></div>
           <div className={styles.info}>
             <label className={styles.avatar}>
-              <img
-                src={
-                  user.avatar ||
-                  'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'
-                }
-                alt='AVATAR'
-              />
-              <input type='file' name='avatar' />
+              <img src={user.avatar || userPlaceholder} alt='AVATAR' />
+              <input type='file' name='avatar' onChange={handleUploadAvatar} />
             </label>
-            <p className={styles.fullname}>{fullName}</p>
-            <button>Cохранить</button>
+            {isEditable ? (
+              <form onSubmit={handleSubmit(handleEditUser)}>
+                <div className={styles.editable}>
+                  <input
+                    {...register('name')}
+                    type='text'
+                    placeholder='Имя'
+                    defaultValue={user.name}
+                  />
+                  <input
+                    {...register('surname')}
+                    type='text'
+                    placeholder='Фамилия'
+                    defaultValue={user.surname}
+                  />
+                </div>
+                <button type='submit'>Cохранить</button>
+              </form>
+            ) : (
+              <>
+                <div className={styles.editable}>
+                  <p>{fullName}</p>
+                </div>
+                <button onClick={() => setIsEditable(true)}>Изменить</button>
+              </>
+            )}
           </div>
         </div>
         <div className={styles.additional}></div>
