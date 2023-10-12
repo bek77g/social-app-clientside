3107ebaa7e81356cdb2cc2d2bc80d3de97149162
bek77g <d.zheenkul@gmail.com>
2023-10-12 14:22:52 +0600

    add: check Profile page content and routing

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,7 +2,12 @@ import { useParams } from 'react-router-dom';
 
 const Profile = () => {
   const { profileId } = useParams();
-  return <div>{profileId}</div>;
+  return (
+    <div>
+      {profileId} - PROFILE PAGE
+      <h1>HELLO</h1>
+    </div>
+  );
 };
 
 export default Profile;
